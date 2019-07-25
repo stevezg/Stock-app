03ad5b5bc761d260b8d2971969a35d04697653ca
Stephen Anderson <anderssj@usc.edu>
2019-07-25 16:19:54 -0700

    adjusts table

--- src/components/Table.js
+++ src/components/Table.js
@@ -25,55 +25,63 @@ export default class Table extends Component {
   }
   render() {
     return (
-      <div>
-        {this.state.moreInfo && (
-          <div>
-            <div style={{ display: 'inline-flex' }}>
-              <h3>More Info</h3>
-              <button
-                style={{
-                  height: '30px',
-                  marginTop: '20px',
-                  width: '100px',
-                  marginlLeft: '10px'
-                }}
-                onClick={this.handleToggle}
-              >
-                Collapse
-              </button>
+      <div className="container">
+        <div>
+          <h2>Stocks</h2>
+          <table>
+            <tbody>
+              <tr>
+                <th>Stock</th>
+                <th>Price</th>
+                <th>Change</th>
+                <th>Percent Change</th>
+              </tr>
+              {this.props.stocks.map((stock, i) => {
+                return (
+                  <tr
+                    key={i}
+                    data-index={i}
+                    onClick={() => this.handleClick(i)}
+                  >
+                    <td>{stock.symbol}</td>
+                    <td>{stock.price}</td>
+                    <td>{stock.change}</td>
+                    <td>{stock.changePercent}</td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+        </div>
+        <div>
+          {this.state.moreInfo && (
+            <div>
+              <div style={{ display: 'inline-flex' }}>
+                <h3>More Info</h3>
+                <button
+                  style={{
+                    height: '30px',
+                    marginTop: '20px',
+                    width: '100px',
+                    marginlLeft: '10px'
+                  }}
+                  onClick={this.handleToggle}
+                >
+                  Collapse
+                </button>
+              </div>
+              <ul style={{ textAlign: 'left' }}>
+                <li>
+                  Volume: {this.props.stocks[this.state.key].volume} shares
+                </li>
+                <li>Open: {this.props.stocks[this.state.key].open}</li>
+                <li>High: {this.props.stocks[this.state.key].high}</li>
+                <li>Low: {this.props.stocks[this.state.key].low}</li>
+                <li>previousClose: {this.props.stocks[this.state.key].previousClose}</li>
+              </ul>
             </div>
-            <ul style={{ textAlign: 'left' }}>
-              <li>Volume: {this.props.stocks[this.state.key].volume} shares</li>
-              <li>Open: {this.props.stocks[this.state.key].open}</li>
-              <li>High: {this.props.stocks[this.state.key].high}</li>
-              <li>Low: {this.props.stocks[this.state.key].low}</li>
-              <li>
-                previousClose: {this.props.stocks[this.state.key].previousClose}
-              </li>
-            </ul>
-          </div>
-        )}
-        <h2>Stocks</h2>
-        <table>
-          <tbody>
-            <tr>
-              <th>Stock</th>
-              <th>Price</th>
-              <th>Change</th>
-              <th>Percent Change</th>
-            </tr>
-            {this.props.stocks.map((stock, i) => {
-              return (
-                <tr key={i} data-index={i} onClick={() => this.handleClick(i)}>
-                  <td>{stock.symbol}</td>
-                  <td>{stock.price}</td>
-                  <td>{stock.change}</td>
-                  <td>{stock.changePercent}</td>
-                </tr>
-              )
-            })}
-          </tbody>
-        </table>
+          )}
+        </div>
       </div>
     )
   }
